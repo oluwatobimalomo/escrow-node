6cd6ffe480712b241af6336a228191b0e6bad086
oluwatobimalomo <malomodaniels@gmail.com>
2026-09-04 14:44:43 +0100

    Add two-factor authentication backend (Better Auth twoFactor plugin)

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -1,10 +1,18 @@
 'use client'
 
 import { createAuthClient } from 'better-auth/react'
-import { siweClient, adminClient } from 'better-auth/client/plugins'
+import { siweClient, adminClient, twoFactorClient } from 'better-auth/client/plugins'
 
 export const authClient = createAuthClient({
-  plugins: [siweClient(), adminClient()],
+  plugins: [
+    siweClient(),
+    adminClient(),
+    twoFactorClient({
+      onTwoFactorRedirect() {
+        window.location.href = '/two-factor'
+      },
+    }),
+  ],
 })
 
 export const { signIn, signUp, signOut, useSession } = authClient
